3f204a039c8bf9df886aefbf65d79b90568c8d0a
WaqasHakeem <waqas@rnssol.com>
2021-03-01 14:12:48 +0500

    staging gtm added

--- packages/mars-theme/src/store/config.js
+++ packages/mars-theme/src/store/config.js
@@ -8,7 +8,8 @@ const AppMode = ["default"];
 const development = "http://18.221.121.169:4000/api";
 const production = "http://18.221.121.169:4000/api";
 const staging = "http://3.15.22.214:4000/api";
-const gtmId = "GTM-PD6TLM2"
+// const gtmId = "GTM-PD6TLM2";
+const gtmId = "GTM-NVMMRLR";
 const websiteLink ="https://staging-web.antlia.io"
 
 let ApiUrl = "";
