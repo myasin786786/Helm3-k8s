85c3cc3926363fdcf96b91869008d8ec2f356562
Bilal Asif <bilal@rnssol.com>
2021-07-29 10:31:58 +0500

    Creative_destruction_Lab removed

--- packages/mars-theme/src/components/OurPartners/OurPartners.js
+++ packages/mars-theme/src/components/OurPartners/OurPartners.js
@@ -1,5 +1,4 @@
 import React from "react";
-import CreativeDestructionLab from '../../assets/logos/Creative_destruction_Lab.png';
 import RNS from '../../assets/logos/rns.png';
 import TrustedChain from '../../assets/logos/trusted_chain.png';
 
@@ -12,13 +11,6 @@ const OurPartners = () => {
             <h2>Our Partners</h2>
           </div>
           <div className="coin-list ourPartners">
-            <div className="single-item">
-              <div className="img-box">
-                <a href="https://www.creativedestructionlab.com/" target="_blank" rel="noopener noreferrer">
-                  <img height="auto" width="100%" src={CreativeDestructionLab} alt="Creative Destruction Lab" />
-                </a>
-              </div>
-            </div>
             <div className="single-item">
               <div className="img-box">
                 <a href="https://www.rnssol.com/" target="_blank" rel="noopener noreferrer">
