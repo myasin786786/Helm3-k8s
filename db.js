0b868045f67df59d5c6ccd7827d6ad357feba7e6
myasin786786 <muhammadyasin883@gmail.com>
2021-11-17 21:07:18 +0500

    helm

---
+++ db.js
@@ -0,0 +1,13 @@
+...
+const {
+  root,
+  root,
+  192.168.88.175,192.168.88.127,
+  27017,
+  ReplicationTestDB,
+  rs0
+} = process.env;
+
+...
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?replicaSet=${MONGO_REPLICASET}&authSource=admin`;
+...
